bc568292938a2f610fc190061c08303e98f6798f
Oleg <n7o.nto@yandex.ru>
2026-09-19 22:44:51 +0300

    Implemented isInside

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <pthread.h>
+
+double area(double r, size_t threads, size_t tests);
+size_t calc(double r, size_t tests, size_t seed);
+bool isInside(double x, double y, double r)
+{
+  return ((x - r) * (x - r) + (y - r) * (y - r)) <= (r * r);
+}
 
 int main()
 {
